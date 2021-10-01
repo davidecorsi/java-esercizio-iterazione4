80bfb746242c2bf2d0864114d4e1fb03352d062e
Davide Corsi <davide.corsi@par-tec.it>
2021-10-01 11:04:18 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+/*
+ * Il package java.util fornisce la classe Random. Leggete la documentazione della classe e create un
+ * applicazione che simuli i lanci di una moneta. Calcolate la percentuale di lanci, all'aumentare dei
+ * lanci la percentuale si dovrebbe stabilizzare al 50%.
+ */
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
