bddb9906b179c1aa909f95343b7c3f6284ac82c8
Davide Corsi <davide.corsi@par-tec.it>
2021-10-01 11:04:46 +0200

    risoluzione

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,18 @@ import java.util.Random;
  */
 public class Main {
 	public static void main(String[] args) {
-
+		int lanci = 1000;
+		int testa = 0;
+		int croce = 0;
+		Random random = new Random();
+		for(int i = 0; i < lanci; i++) {
+			if(random.nextBoolean()) {
+				testa++;
+			} else {
+				croce++;
+			}
+		}
+		System.out.println("La percentuale di lanci risultati testa sono:" + (float) testa/lanci*100);
+		System.out.println("La percentuale di lanci risultati croce sono:" + (float) croce/lanci*100);
 	}
 }
